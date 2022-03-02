280b4510b58312fa13946ff6da3c4590e75e2169
1134412480zx <2776835685@qq.com>
2022-03-02 18:34:56 +0800

    首次提交

---
+++ normalball.cpp
@@ -0,0 +1,6 @@
+#include "normalball.h"
+
+NormalBall::NormalBall()
+{
+
+}
---
+++ normalball.h
@@ -0,0 +1,11 @@
+#ifndef NORMALBALL_H
+#define NORMALBALL_H
+
+
+class NormalBall : public GameMoveObject
+{
+public:
+    NormalBall();
+};
+
+#endif // NORMALBALL_H
